f04ff60b93cc2074f579ef39219378e031172a6c
renatohss <renatohss@yahoo.com.br>
2020-02-04 01:56:08 -0200

    Projeto finalizado

---
+++ monopoly.py
@@ -0,0 +1,139 @@
+from settings import Player, Board
+from random import randint
+from pprint import pprint
+import operator
+
+def roll_d6():
+    return randint(1, 6)
+
+
+def buy_space(board, player):
+    index = player.position
+    board[index]['owner'] = player
+    player.spaces_owned.append(index)
+    player.cash -= board[index]['value']
+
+
+def pay_rent(board, player):
+    index = player.position
+    player.cash -= board[index]['rent']
+    board[index]['owner'].cash += board[index]['rent']
+
+
+def check_status(player):
+    if player.cash >= 0:
+        return True
+    else:
+        return False
+
+
+
+def start_game(max_rounds):
+    board = Board().board
+    players = {
+        1: Player('player1', 'impulsive', 300),
+        2: Player('player2', 'demanding', 300),
+        3: Player('player3', 'cautious', 300),
+        4: Player('player4', 'random', 300)
+    }
+
+    _round = 1
+    active_players = len(players)
+        
+    while _round <= max_rounds:
+        for num, current_player in players.items():
+            if current_player.status == 'active':
+                dice = roll_d6()
+                position = current_player.position + dice
+                current_player.position = position if position <= 20 else 1
+                board_space = board[current_player.position]
+
+                if not board_space['owner']:
+                    if current_player.personality == 'impulsive':
+                        if current_player.cash > board_space['value']:
+                            buy_space(board, current_player)
+
+                    elif current_player.personality == 'demanding':
+                        if board_space['rent'] > 50 and current_player.cash > board_space['value']:
+                            buy_space(board, current_player)
+
+                    elif current_player.personality == 'cautious':
+                        if (current_player.cash - board_space['value']) > 80:
+                            buy_space(board, current_player)
+
+                    else:
+                        if randint(1,2) == 1 and current_player.cash > board_space['value']:
+                            buy_space(board, current_player)
+
+                else:
+                    pay_rent(board, current_player)
+
+                status = check_status(current_player)
+                if not status:
+                    for num in current_player.spaces_owned:
+                        board[num]['owner'] = None
+                    current_player.spaces_owned = []
+                    current_player.status = 'game_over'
+                    active_players -= 1
+
+        if active_players == 1:
+            break
+
+        _round += 1
+        
+    winner = [players[index] for index in players if players[index].status == 'active']
+    return {
+        'winner': winner,
+        'total_rounds': _round,
+        'timeout': True if _round >= max_rounds else False
+    }
+    
+    
+def main():
+    win_count = {
+        'player1': [0],
+        'player2': [0],
+        'player3': [0],
+        'player4': [0]
+    }
+
+    total_rounds = 0
+    total_sims = 300
+    total_timeout = 0
+    max_rounds = 1000
+
+    count = 1
+
+    print(f'Maximum number of rounds before timeout: {max_rounds}')
+    print(f'Running {total_sims} simulations...')
+
+    while count <= total_sims:
+        payload = start_game(max_rounds)
+        for player in payload['winner']:
+            win_count[player.name][0] += 1
+            win_count[player.name].append(player.personality)
+        total_rounds += payload['total_rounds']
+        if payload['timeout']:
+            total_timeout += 1
+        count += 1
+
+    max_win = 0
+    max_player = ''
+    for player, win in win_count.items():
+        if win[0] > max_win:
+            max_win = win[0]
+            max_player = player
+
+    print(f'\nResults:')
+    print(f'Avg. Rounds: {round(total_rounds/total_sims, 2)}')
+    print(f'Total of matches ending with timeout: {total_timeout}')
+    print(f'Percentage of victory:\n \
+            {win_count["player1"][1]}: {round((win_count["player1"][0]/total_sims)*100, 2)}%\n \
+            {win_count["player2"][1]}: {round((win_count["player2"][0]/total_sims)*100, 2)}%\n \
+            {win_count["player3"][1]}: {round((win_count["player3"][0]/total_sims)*100, 2)}%\n \
+            {win_count["player4"][1]}: {round((win_count["player4"][0]/total_sims)*100, 2)}%\n')
+    print(f'{max_player} won most times, with {max_win} victories')
+            
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,42 @@
+from random import randint
+
+class Board:
+    '''
+    This class creates a instance of the board with random values withing the range 100 - 350, calculates the rent for each one as 25%
+    of the face value, sets the owner name to None and then builds the board as a dict object
+    '''
+    def __init__(self):
+        self.board = self.create_board()
+    
+    def create_board(self):
+        board = {}
+        values = [randint(150, 500) for x in range(0, 20)]
+        rent = [randint(10, 60) for value in values]
+        zip_list = zip(values, rent)
+        count = 1
+        for item in zip_list:
+            board[count] = {
+                'value': item[0],
+                'rent': item[1],
+                'owner': None
+            }
+            count += 1
+        return board
+
+
+class Player:
+    '''
+    This class creates a instance of a player object with name, personality, initial cash, sets the 
+    initial position as 1 and status to active
+    '''
+    def __init__(self, name, personality, start_cash):
+        self.name = name
+        self.personality = personality
+        self.cash = start_cash
+        self.position = 1
+        self.spaces_owned = []
+        self.status = 'active'
+
+    def __str__(self):
+        return self.name
+
